86480fc96cb0368d29e0bf0d80160f9b8651e7d6
mltavares <miguel-tavares10@hotmail.com>
2023-10-28 00:09:09 +0100

    makefile e inicio do jogoUI

---
+++ src/jogoUI.c
@@ -0,0 +1,12 @@
+#include "jogoUI.h"
+
+struct jogoUI j[20];
+
+void comandosJogador(){
+    //utilizar strtok para validar os comandos do jogador
+}
+
+
+int main(int argc, char *argv[]){
+
+}
---
+++ src/jogoUI.h
@@ -0,0 +1,17 @@
+#ifndef JOGOUI_H
+#define JOGOUI_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+struct user
+{
+    char nome[50];
+};
+
+
+#endif
