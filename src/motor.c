dfeacae3f895776127155055ba4efce81c965255
mltavares <miguel-tavares10@hotmail.com>
2023-11-01 19:04:54 +0000

    mudancas no motor e acrescentei o bot

--- src/motor.c
+++ src/motor.c
@@ -5,39 +5,63 @@ void comandosMotor(){
     char comandos[50];
     char *token;
 
-    printf("Comando: ");
-
     do{
+
+        printf("\nComando: ");
+        fflush(stdout);  
+
         fgets(comandos, 50, stdin);
+
+        size_t len = strlen(comandos);
+        if (len > 0 && comandos[len-1] == '\n') {
+            comandos[len-1] = '\0';
+        }
         token = strtok(comandos, " ");
 
-        if(strcmp(token, "users")==0){
-            printf("\nusers detetado\n");
+        if(strcmp(token,"users")==0){
+            printf("users detetado\n");
         }
-        if(strcmp(token, "kick")==0){
+        else if(strcmp(token,"kick")==0){
             char *name_token = strtok(NULL, " ");
-            printf("\nO utilizador %s foi kickado\n", name_token);
+            if(name_token == NULL){
+                printf("Utilizador invalido\n");
+            }
+            else{
+                printf("O utilizador %s foi kickado\n", name_token);
+            }
         }
-        if(strcmp(token, "bots")==0){
-            printf("\nbots detetado\n");
+        else if(strcmp(token,"bots")==0){
+            printf("bots detetado\n");
         }
-        if(strcmp(token, "bmov")==0){
-            printf("\nbmov detetado\n");
+        else if(strcmp(token,"bmov")==0){
+            printf("bmov detetado\n");
         }
-        if(strcmp(token, "rbm")==0){
-            printf("\nrbm detetado\n");
+        else if(strcmp(token,"rbm")==0){
+            printf("rbm detetado\n");
         }
-        if(strcmp(token, "begin")==0){
-            printf("\nbegin detetado\n");
+        else if(strcmp(token,"begin")==0){
+            printf("begin detetado\n");
         }
-        if(strcmp(token, "end")==0){
-            printf("\nend detetado\n");
+        else if(strcmp(token,"end")==0){
+            printf("end detetado\n");
             exit(1);
+        }
+        else if(strcmp(token,"test_bot")==0){
+            //lancar um bot
+            printf("A lancar bot...\n");
+        }
+        else{
+            printf("Comando invalido\n");
         }
 
 
 
     }while(1);
 
+}
+
+int main(int argc, char *argv[]){
+    comandosMotor();
+    return 0;
 }
 
