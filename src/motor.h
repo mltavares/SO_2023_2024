3e29165877012bbc12cd1552fdbb9fe950584e26
Quelhas11 <85692492+Quelhas11@users.noreply.github.com>
2024-01-01 19:54:38 +0000

    Update motor.h

--- src/motor.h
+++ src/motor.h
@@ -1,3 +1,4 @@
+  GNU nano 7.2                                                                                          motor.h                                                                                                    
 #ifndef MOTOR_H
 #define MOTOR_H
 
@@ -10,84 +11,14 @@
 #include <time.h>
 #include <sys/types.h>
 #include <signal.h>
-#include "jogoUI.h"
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <errno.h>
 #include <dirent.h>
 #include <pthread.h>
-    
-#define MAX_PLAYERS 5
-#define MAX_BMOVS 5
-#define MAX_BOTS 10
-#define PLAYER_NAME_SIZE 20
-#define MAX_PIPE_SIZE 12
-#define MAX_HEIGHT 16
-#define MAX_WIDTH 80
-#define LINHAS 16
-#define COLUNAS 40
+#include "comuns.h"
 
 static int child_exit_status;
-
-typedef struct {
-    Jogador jogadores[5];
-    int nivel;
-    char maze[LINHAS][COLUNAS];  
-} Jogo;
-
-typedef struct {
-    char name[PLAYER_NAME_SIZE];
-    char pipe[MAX_PIPE_SIZE];
-    pid_t pid;
-    char icone;
-    int xCoordinate;
-    int yCoordinate;
-    int isPlaying;
-} Player;
-
-typedef struct {
-    Player array[MAX_PLAYERS];
-    int playerFd[MAX_PLAYERS];
-    int nPlayers;
-} PlayerArray;
-
-typedef struct {
-    int interval;
-    int duration;
-    pthread_t *botThread;
-} Bot;
-
-typedef struct {
-    int interval;
-    int duration;
-} BotToken;
-
-typedef struct {
-    Bot bots[MAX_BOTS];
-    int nBots;
-} BotArray;
-
-typedef struct {
-    int x;
-    int y;
-} Bmov;
-
-typedef struct {
-    Bmov bmovs[MAX_BMOVS];
-    int nbmovs;
-} BmovArray;
-
-typedef struct {
-    char array[MAX_HEIGHT][MAX_WIDTH];
-} Map;
-
-typedef struct {
-    PlayerArray *players;
-    BotArray *bots;
-    BmovArray *bmovs;
-    Map *map;
-    int keyboardFeed;
-} KeyboardHandlerToken;
 
 void comandoUsers(KeyboardHandlerToken *token);
 void comandoBots(KeyboardHandlerToken *token);
